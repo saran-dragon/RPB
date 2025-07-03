75b3e691c2d89b9cf8afedc26fed6892fcc90a81
saran-dragon <dragonkp431@gmail.com>
2025-07-03 22:47:17 +0530

    Update Gallery.tsx

--- admin/src/pages/Gallery.tsx
+++ admin/src/pages/Gallery.tsx
@@ -1,4 +1,5 @@
-import React, { useEffect, useState, ChangeEvent, FormEvent } from 'react';
+import React, { useEffect, useState } from 'react';
+import type { ChangeEvent, FormEvent } from 'react';
 import axios from 'axios';
 
 interface GalleryItem {
